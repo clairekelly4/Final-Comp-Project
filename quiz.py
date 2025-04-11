138470a105cf50fc96305388c833b8c33139fe69
claire kelly <clairekelly@claires-MacBook-Air.local>
2025-04-11 16:12:21 -0400

    added new files

---
+++ quiz.py
@@ -0,0 +1,14 @@
+
+statements = [
+    {"statement": "I like having a clear plan before beginning work.", "trait" : "Planner"},
+    {"statement": "I like to check in regularly with my group members.", "trait": "Accountable"},
+    {"statement": "I like to get started on projects as early as possible.", "trait": "Proactive"},
+    {"statement": "I value flexibility and am open to new ideas.", "trait": "Adaptive"},
+    {"statement": "I feel most energized when working with other people.", "trait": "Extroverted"},
+    {"statement": "I feel more productive when I can work independently.", "trait": "Introverted"},
+    {"statement": "I feel stressed if things are unorganized or uncertain.", "trait": "Need For Order"},
+    {"statement": "I prefer frequent communication, even for small updates.", "trait": "Communicative"},
+    {"statement": "I naturally take the lead in group settings.", "trait": "Leader"},
+    {"statement": "I stay calm and composed when there's disagreement.", "trait": "Calm Under Pressure"}
+]
+
