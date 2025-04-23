7de9aeeb70050ac76eaf68599c6773177421f3d2
sydney-sr <sydneysr24@gmail.com>
2025-04-23 18:53:48 -0400

    score pair function

--- match.py
+++ match.py
@@ -15,3 +15,24 @@ def get_difference(responses1: List[int], responses2: List[int], indices: List[i
     :return: total difference for all questions
     """
     return sum(abs(responses1[i] - responses2[i]) for i in indices)
+
+def score_pair(student1: Dict, student2: Dict) -> float:
+   """
+   gives a score to a pair of students by measuring how similar they are in work style and how different they are in leadership and coding
+   then subtracts similarity from diversity -> higher score = better match
+   :param student1: first student
+   :param student2: second student
+   :return:
+   """
+   # extract quiz responses for both students
+   responses1, responses2 = student1['responses'], student2['responses']
+
+   # measure similarity in work style (lower difference = more similar)
+   similarity = get_difference(responses1, responses2, work_style_indices)
+
+   # measure diversity in leadership and coding (higher difference = more diverse)
+   diversity = get_difference(responses1, responses2, leadership_indices + coding_indices)
+
+   # combine both metrics; prioritize diverse strengths but similar work styles
+   return diversity - similarity # higher score = better
+
